f33fed23ad4e68ef9beb69de1b908cceefa58f24
DominikGovorc <79502901+DominikGovorc@users.noreply.github.com>
2021-04-13 10:22:03 +0200

    vjezba
    yes

---
+++ Dominik 2E TileMaps/Assets/MenuScripts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripts : MonoBehaviour
+{
+    public Canvas pauseMenu;
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void Quit()
+        {
+            Application.Quit();
+        }
+        public void Resume()
+        {
+            Time.timeScale=1;
+            pauseMenu.enabled=false;
+        }
+        public void MainMenu()
+        {
+            Resume();
+            SceneManager.LoadScene(0);
+        }
+}
---
+++ Dominik 2E TileMaps/Assets/SingletonKlasa.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonKlasa : MonoBehaviour
+{
+    public GameObject gameUI;
+    [SerializeField]
+    public int level = 1;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    public void LevelUp()
+    {
+        level++;
+    }
+    public static SingletonKlasa Instance
+    {
+        get; set;
+    }
+}
