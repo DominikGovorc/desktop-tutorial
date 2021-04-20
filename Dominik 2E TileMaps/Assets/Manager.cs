1faa9bdbd1617b77da54d4b1f380ed98796c3f6e
MarkoPujic <79627058+MarkoPujic@users.noreply.github.com>
2021-04-20 12:38:26 +0200

    a
    a

---
+++ Dominik 2E TileMaps/Assets/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject energyBall;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(energyBall, new Vector2(-20, Random.Range(-4,4)), Quaternion.identity);
+        }
+    }
+}
